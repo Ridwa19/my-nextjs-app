544d919ea457d438dc6f93ab18370951d8be656f
Ridwan <ridwan@tabaarakict.org>
2025-04-09 07:56:18 +0300

    changes

---
+++ src/app/about/page.js
@@ -0,0 +1,17 @@
+export default function About(){
+    return (
+
+        <main className="flex items-center justify-center min-h-screen bg-blue-50">
+          < div className="text-center">
+          <h1 className="text-4xl font-bold text-blue-700">
+                about me 
+            </h1>
+
+            <p className="mt-4 text-lg text-gray-600">hello i am me i new @next.js</p>
+
+            </div> 
+        </main>
+
+    )
+
+}
---
+++ src/app/contact/page.js
@@ -0,0 +1,13 @@
+export default function Contact() {
+    return (
+      <main className="flex items-center justify-center min-h-screen bg-green-50">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-green-700">Contact Me</h1>
+          <p className="mt-4 text-lg text-gray-600">
+            You can reach me at: <strong>ridwaana88@gmail.com</strong>
+          </p>
+        </div>
+      </main>
+    );
+  }
+  
